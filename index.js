a5492cfad029c923c9654c4fefee09da2246712a
John Zirnkilton <102177316+JZ-BRMGT@users.noreply.github.com>
2024-02-14 13:04:20 -0500

    asdfasfsaf

---
+++ index.js
@@ -0,0 +1,7 @@
+// Description: this file is the entry point of the application
+// This is to just go to SEC and download the files
+// URL: https://www.sec.gov/data/foiadocsfailsdatahtm
+
+const puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('fs');
